de85cd6d9e64e34576e2115c7cf2a4db261a1f4c
Gino2B <gbaybay30@gmail.com>
2021-07-20 16:41:27 -0400

    Added a bunch of user files(routes, controllers), and authentication

--- routes/albums.js
+++ routes/albums.js
@@ -1,12 +1,13 @@
-import { Router } from "express"
-import * as controllers from "../controllers/albums.js"
+import { Router } from "express";
+import * as controllers from "../controllers/albums.js";
+import restrict from "../helpers/restrict.js";
 
-const router = Router()
+const router = Router();
 
-router.get("/albums", controllers.getAlbums)
-router.get("/albums/:id", controllers.getAlbum)
-router.post("/albums", controllers.createAlbum)
-router.put("/albums/:id", controllers.updateAlbum)
-router.delete("/albums/:id", controllers.deleteAlbum)
+router.get("/albums", controllers.getAlbums);
+router.get("/albums/:id", controllers.getAlbum);
+router.post("/albums", restrict, controllers.createAlbum);
+router.put("/albums/:id", restrict, controllers.updateAlbum);
+router.delete("/albums/:id", restrict, controllers.deleteAlbum);
 
-export default router
+export default router;
