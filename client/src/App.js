eef2e1d63e7cfbfefaf23d6846f152d333cd2ac3
Gino2B <gbaybay30@gmail.com>
2021-07-21 11:15:38 -0400

    Created services for getting album list, created path in app.js to display, set up api config file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-
-import './App.css';
+import "./App.css";
+import Albums from "./screens/Albums/Albums.jsx";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route path="/albums">
+          <Albums />
+        </Route>
+      </Switch>
     </div>
   );
 }
