4bc52792d8286070190fc3c61084398dc8859b39
Gino2B <gbaybay30@gmail.com>
2021-07-22 09:32:14 -0400

    Created user services with no products

---
+++ client/src/services/users.js
@@ -0,0 +1,47 @@
+import api from "/apiConfig";
+import jwtDecode from "jwt-decode";
+
+export const signUp = async (credentials) => {
+  try {
+    const resp = await api.post("/sign-up", credentials);
+    localStorage.setItem("token", resp.data.token);
+    console.log(resp.data.token);
+    const user = jwtDecode(resp.data.token);
+    console.log(user);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signIn = async (credentials) => {
+  try {
+    const resp = await api.post("/sign-in", credentials);
+    localStorage.setItem("token", resp.data.token);
+    console.log(resp.data.token);
+    const user = jwtDecode(resp.data.token);
+    console.log(user);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signOut = async () => {
+  try {
+    localStorage.removeItem("token");
+    return true;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const verifyUser = async () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const res = await api.get("/verify");
+    console.log(res.data);
+    return res.data;
+  }
+  return false;
+};
