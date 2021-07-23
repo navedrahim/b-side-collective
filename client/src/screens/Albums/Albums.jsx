4ba59c1ad08b056a7c27197728c49b71abe6cc19
Gino2B <gbaybay30@gmail.com>
2021-07-23 12:17:47 -0400

    Fixed warnings and errors, and fixed the styling issue for the album screens

--- client/src/screens/Albums/Albums.jsx
+++ client/src/screens/Albums/Albums.jsx
@@ -18,16 +18,16 @@ function Albums(props) {
     <Layout user={props.user}>
       <div className="album-container">
         {albums.map((album) => (
-          <div className="album-cards">
+          <div className="album-cards" key={album._id}>
             <Link to={`/albums/${album._id}`}>
-              <div className="album-card" key={album._id}>
+              <div className="albums-card">
                 <img
-                  className="album-cover"
+                  className="albums-cover"
                   src={album.imageURL}
                   alt={album.album}
                 />
-                <div className="album-name">{album.album}</div>
-                <div className="album-artist">{album.artist}</div>
+                <div className="albums-name">{album.album}</div>
+                <div className="albums-artist">{album.artist}</div>
               </div>
             </Link>
           </div>
