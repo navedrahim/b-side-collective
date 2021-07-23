0bebfafc6e157595d3c8a6325f3579ab52952a04
navedrahim <nrah1988@gmail.com>
2021-07-23 16:27:03 -0400

    styled edit screen

--- client/src/screens/AlbumEdit/AlbumEdit.jsx
+++ client/src/screens/AlbumEdit/AlbumEdit.jsx
@@ -1,4 +1,5 @@
 import { editAlbum, getAlbum } from "../../services/albums";
+import "./AlbumEdit.css";
 import { useParams, Redirect } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
@@ -47,78 +48,84 @@ function AlbumEdit(props) {
 
   return (
     <Layout user={props.user}>
-      <form className="edit-form" onSubmit={handleSubmit}>
-        <input
-          className="input-album"
-          placeholder="Album"
-          value={album.album}
-          name="album"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <input
-          className="input-artist"
-          placeholder="Artist"
-          value={album.artist}
-          name="artist"
-          required
-          onChange={handleChange}
-        />
-        <input
-          className="input-release"
-          placeholder="Release Date"
-          value={album.release_date}
-          name="release_date"
-          required
-          onChange={handleChange}
-        />
-        <input
-          className="input-price"
-          placeholder="Price"
-          value={album.price}
-          name="price"
-          required
-          onChange={handleChange}
-        />
-        <input
-          className="input-image"
-          placeholder="Image Link"
-          value={album.imageURL}
-          name="imageURL"
-          required
-          onChange={handleChange}
-        />
-        <input
-          className="input-genre"
-          placeholder="Genre"
-          value={album.genre}
-          name="genre"
-          required
-          onChange={handleChange}
-        />
-        <input
-          className="input-label"
-          placeholder="Label"
-          value={album.label}
-          name="label"
-          required
-          onChange={handleChange}
-        />
-        <textarea
-          className="textarea-description"
-          rows={10}
-          placeholder="Description"
-          value={album.description}
-          name="description"
-          required
-          onChange={handleChange}
-        />
-        <img src={album.imageURL} alt={album.album}></img>
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
+      <div className="album-edit">
+        <form className="edit-form" onSubmit={handleSubmit}>
+          <input
+            className="input-album"
+            placeholder="Album"
+            value={album.album}
+            name="album"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className="input-artist"
+            placeholder="Artist"
+            value={album.artist}
+            name="artist"
+            required
+            onChange={handleChange}
+          />
+          <input
+            className="input-release"
+            placeholder="Release Date"
+            value={album.release_date}
+            name="release_date"
+            required
+            onChange={handleChange}
+          />
+          <input
+            className="input-price"
+            placeholder="Price"
+            value={album.price}
+            name="price"
+            required
+            onChange={handleChange}
+          />
+
+          <input
+            className="input-genre"
+            placeholder="Genre"
+            value={album.genre}
+            name="genre"
+            required
+            onChange={handleChange}
+          />
+          <input
+            className="input-label"
+            placeholder="Label"
+            value={album.label}
+            name="label"
+            required
+            onChange={handleChange}
+          />
+          <textarea
+            className="textarea-description"
+            rows={10}
+            placeholder="Description"
+            value={album.description}
+            name="description"
+            required
+            onChange={handleChange}
+          />
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+        <div className="image-container">
+          <img className="edit-image" src={album.imageURL} alt={album.album} />
+          <textarea
+            className="input-image-link"
+            placeholder="Image Link"
+            value={album.imageURL}
+            name="imageURL"
+            rows={2}
+            required
+            onChange={handleChange}
+          />
+        </div>
+      </div>
     </Layout>
   );
 }
